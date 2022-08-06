ee24899fabc89f191adb876b1f866ccaa10a48b2
Giorgi Dzigoshvili <georgedzigo@gmail.com>
2022-08-06 17:17:12 +0300

    1.1 - Theme files

---
+++ app/design/frontend/PixelPro/ThemePro/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2022 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Pixelpro/ThemePro',
+    __DIR__
+);
