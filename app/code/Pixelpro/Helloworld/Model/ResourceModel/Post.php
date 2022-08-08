3e48769a78376e99bb6303fc4b00942344be12cc
Giorgi Dzigoshvili <georgedzigo@gmail.com>
2022-08-08 14:46:57 +0300

    2.2 - Developing the model

--- app/code/Pixelpro/Helloworld/Model/ResourceModel/Post.php
+++ app/code/Pixelpro/Helloworld/Model/ResourceModel/Post.php
@@ -1,19 +1,18 @@
 <?php
-
 namespace Pixelpro\Helloworld\Model\ResourceModel;
-
-use Magento\Framework\Model\ResourceModel\Db\Context;
-
 class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
 
-    public function __construct(Context $context)
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context
+    )
     {
-        parent::_construct($context);
+        parent::__construct($context);
     }
 
     protected function _construct()
     {
         $this->_init('pixelpro_helloworld_post', 'post_id');
     }
+
 }
